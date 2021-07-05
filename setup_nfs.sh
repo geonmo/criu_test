25c5b6374d95503bcb087c135c67c6f728c392bb
Geonmo Ryu <geonmo@kisti.re.kr>
2021-07-05 04:48:39 +0000

    NFS 셋업 설정 추가.

---
+++ setup_nfs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NODE=${HOSTNAME::5}
+
+if [ ! -d "/mnt/share" ];
+then
+	sudo mount -p /mnt/share
+fi
+
+if [ -s "/etc/exports" ];
+then
+	echo "/mnt/share *(no_subtree_check)" | sudo tee -a /etc/exports
+fi
+
+if [ $NODE == "node0" ];
+then
+	sudo firewall-cmd --add-service=nfs --permanent
+	sudo firewall-cmd --reload
+else
+	echo "Wrong node"
+fi
+
