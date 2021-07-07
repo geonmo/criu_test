cf0bfe8d00bcf281efee83a576a029b72ad6eefe
Geonmo Ryu <geonmo@kisti.re.kr>
2021-07-07 07:04:10 +0000

    단순 condor job 관련 내용 정리

--- 02.condor_job/test.sh
+++ 02.condor_job/test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-setsid ./test_sleep.sh &
+./test_sleep.sh > output.log 2> error.log &
+#./test_sleep.sh & 
 
-sleep 15
+sleep 4
 
-mkdir -p $HOME/dump/01
-cd $HOME/dump/01
+#mkdir -p $HOME/dump/01
+#cd $HOME/dump/01
 
 
 
@@ -22,21 +23,5 @@ ls -l /proc/${TPID}/fd
 cat /proc/${TPID}/fdinfo/1
 
 
-echo "$_CONDOR_SCRATCH_DIR"
-echo "$$"
 
-STDOUT_INODE=$(ls -il $_CONDOR_SCRATCH_DIR/_condor_stdout| cut -d' ' -f1)
-
-echo "_stdout's inode ; ${STDOUT_INODE}"
-#criu dump -t $(pgrep test_sleep.sh) -j --link-remap -o $HOME/dump/01/output.log
-criu dump -t ${TPID} -j --external pipe[${STDOUT_INODE}] -o $HOME/dump/01/output.log
-
-
-#sleep 5
-
-
-#env
-
-#cat /etc/*-release
-
-#cat /proc/$$/mountinfo
+criu dump -t ${TPID} -v5 -j --evasive-devices --leave-running 
