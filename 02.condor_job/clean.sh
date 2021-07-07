3bebdada950244b085fecf7d0f409075ad71dc43
Geonmo Ryu <geonmo@kisti.re.kr>
2021-07-07 01:15:44 +0000

    조정작업 진행

---
+++ 02.condor_job/clean.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -rf job*.{err,out}
--- 02.condor_job/test.sh
+++ 02.condor_job/test.sh
@@ -1,3 +1,42 @@
 #!/bin/bash
 
-./test_sleep.sh
+setsid ./test_sleep.sh &
+
+sleep 15
+
+mkdir -p $HOME/dump/01
+cd $HOME/dump/01
+
+
+
+TPID=$(pgrep test_sleep.sh)
+lsof -p ${TPID}
+
+echo "test.sh's FD"
+ls -l /proc/$$/fd
+cat /proc/$$/fdinfo/1
+
+
+echo "test_sleep.sh's FD"
+ls -l /proc/${TPID}/fd
+cat /proc/${TPID}/fdinfo/1
+
+
+echo "$_CONDOR_SCRATCH_DIR"
+echo "$$"
+
+STDOUT_INODE=$(ls -il $_CONDOR_SCRATCH_DIR/_condor_stdout| cut -d' ' -f1)
+
+echo "_stdout's inode ; ${STDOUT_INODE}"
+#criu dump -t $(pgrep test_sleep.sh) -j --link-remap -o $HOME/dump/01/output.log
+criu dump -t ${TPID} -j --external pipe[${STDOUT_INODE}] -o $HOME/dump/01/output.log
+
+
+#sleep 5
+
+
+#env
+
+#cat /etc/*-release
+
+#cat /proc/$$/mountinfo
