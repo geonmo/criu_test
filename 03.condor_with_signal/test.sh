f1216fcd09ab7aeee3ed2915c710aae8af4a8c16
Geonmo Ryu <geonmo@kisti.re.kr>
2021-07-13 01:03:18 +0000

    Remove unnecessary centos8.sif file.

--- 03.condor_with_signal/test.sh
+++ 03.condor_with_signal/test.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
+trap ReceiveCheckPointSignal SIGUSR2
+
+function ReceiveCheckPointSignal() {
+	TPID=$(pgrep test_sleep.sh)
+	criu dump -t ${TPID} -v5 -j #--evasive-devices --leave-running 
+}
+
+
+
 ./test_sleep.sh > output.log 2> error.log &
-#./test_sleep.sh & 
-
-sleep 4
-
-#mkdir -p $HOME/dump/01
-#cd $HOME/dump/01
+wait
 
 
 
-TPID=$(pgrep test_sleep.sh)
-lsof -p ${TPID}
-
-echo "test.sh's FD"
-ls -l /proc/$$/fd
-cat /proc/$$/fdinfo/1
 
 
-echo "test_sleep.sh's FD"
-ls -l /proc/${TPID}/fd
-cat /proc/${TPID}/fdinfo/1
-
-
-
-criu dump -t ${TPID} -v5 -j --evasive-devices --leave-running 
