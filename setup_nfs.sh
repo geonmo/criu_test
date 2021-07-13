71ca9586a8376daf324cf7e17f8accf495e49d7e
Geonmo Ryu <geonmo@kisti.re.kr>
2021-07-13 06:14:53 +0000

    NFS Setup 수정

--- setup_nfs.sh
+++ setup_nfs.sh
@@ -7,18 +7,15 @@ then
 	sudo mkdir -p /mnt/share
 fi
 
-if [ -s "/etc/exports" ];
-then
-	echo "/mnt/share *(no_subtree_check)" | sudo tee -a /etc/exports
-fi
 
 if [ $NODE == "node0" ];
 then
-    sudo dnf install -y nfs-utils
+    	sudo dnf install -y nfs-utils
+	echo "/mnt/share *(rw,no_subtree_check)" | sudo tee -a /etc/exports
 	sudo firewall-cmd --add-service=nfs --permanent
 	sudo firewall-cmd --reload
 else
-    sudo dnf install -y nfs-utils
+    	sudo dnf install -y nfs-utils
 	echo "192.168.10.3:/mnt/share /mnt/share nfs defaults 0 0 " | sudo tee -a /etc/fstab
 fi
 
