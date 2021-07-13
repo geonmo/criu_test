edc35e65ffef8a8629d6974e2599f0571de6481e
Geonmo Ryu <geonmo@kisti.re.kr>
2021-07-13 02:20:59 +0000

    디렉토리 이름 변경

--- 03.signal_handling/test.sh
+++ 03.signal_handling/test.sh
@@ -3,14 +3,14 @@
 trap ReceiveCheckPointSignal SIGUSR2
 
 function ReceiveCheckPointSignal() {
-	TPID=$(pgrep test_sleep.sh)
 	criu dump -v5 -j -t ${TPID} #--evasive-devices #--leave-running 
+    exit 85
 }
 
 
 if [ -s stats-dump ]; then
 	setsid -f criu restore -j
-	#wait $(pgrep test_sleep.sh)
+	TPID=$(pgrep test_sleep.sh)
 	while true
 	do
 		echo "Monitoring ${TPID} procces"
@@ -22,7 +22,7 @@ if [ -s stats-dump ]; then
 	done
 else
 	setsid -f ./test_sleep.sh #> sleep_output.log 2> sleep_error.log & 
-	#wait $(pgrep test_sleep.sh)
+	TPID=$(pgrep test_sleep.sh)
 	while true
 	do
 	    echo "Monitoring ${TPID} procces"
