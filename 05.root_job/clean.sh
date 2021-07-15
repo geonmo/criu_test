d42b1f5faa556329e0e1eeb8e9d02f169dad928e
Geonmo Ryu <geonmo@kisti.re.kr>
2021-07-15 01:59:04 +0000

    ROOT 프로그램에서의 criu 작동 여부.

---
+++ 05.root_job/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -rf job*.{err,out}
+rm -rf *.img
+rm -f stats-{dump,restore}
+rm -f h1.root
---
+++ 05.root_job/test_root.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+from ROOT import *
+
+f1 = TFile.Open("h1.root","recreate")
+
+
+h1 = TH1F("his","hist",100,0,100)
+
+rand3 = TRandom3()
+rand3.SetSeed(time(0))
+
+
+for x in range(100):
+    value = int(rand3.Rndm()*100%100)
+    print(f"Fill {value} at {x}")
+
+    h1.Fill(x,value)
+    sleep(2) 
+
+
+h1.Write()
+
