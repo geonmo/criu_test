aaa337135f157c488cdcbf5ea9d11a8e8e68c3ba
Geonmo Ryu <geonmo@kisti.re.kr>
2021-07-15 00:39:08 +0000

    Code cleaning.

--- 04.htcondor_with_signal/test.sh
+++ 04.htcondor_with_signal/test.sh
@@ -6,11 +6,12 @@ function ReceiveCheckPointSignal() {
 	TPID=$(pgrep test_sleep.sh)
 
 	criu dump -v5 -j -t ${TPID} -o dump.log --evasive-devices #--leave-running
-        #if [ ! -s workdir ]; then	
 	echo "$(pwd)" > workdir 
-	echo ${TPID} > tpid
-	#fi
+        if [ ! -s tpid ]; then	
+		echo ${TPID} > tpid
+	fi
     	tar -czvf dump.tar.gz *.img stats* sleep*.log workdir tpid
+
 	## Debuging
 	sudo chown geonmo.geonmo dump.log
 	cp dump.log /mnt/share/geonmo
@@ -25,14 +26,8 @@ if [ -s dump.tar.gz ]; then
         tar -zxvf dump.tar.gz
 	WORKDIR=$(cat workdir)
 	sudo ln -Tfs $(pwd) ${WORKDIR}
-	#cd ${WORKDIR}
          	
-	
-	#setsid -f criu restore -j
 	criu restore -d -j --inherit-fd "fd[7]:${WORKDIR:1}/sleep_output.log" --inherit-fd "fd[8]:${WORKDIR:1}/sleep_error.log" --inherit-fd "fd[9]:${WORKDIR:1}/test_sleep.sh" 7>> sleep_output.log 8>>sleep_error.log 9> test_sleep.sh
-	#sleep 2
-	#criu restore -j &
-	#TPID=i$!
 	TPID=$(cat tpid)
 	while true
 	do
@@ -45,20 +40,8 @@ if [ -s dump.tar.gz ]; then
 		sleep 1
 	done
 else
-	#setsid -f ./test_sleep.sh > sleep_output.log 2> sleep_error.log < /dev/null 
 	./test_sleep.sh > sleep_output.log 2> sleep_error.log < /dev/null &
-        #wait $!	
 	TPID=$!
 	wait ${TPID}
-
-	#while true
-	#do
-	#    echo "Monitoring ${TPID} procces"
-	#    if ! pgrep -x test_sleep.sh
-        #then
-        #   exit 0   
-	#fi
-	#   sleep 1
-	#done
 fi
 
